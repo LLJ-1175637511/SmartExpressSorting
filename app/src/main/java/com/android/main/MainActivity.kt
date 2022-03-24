6205436dba4f8a041ae5536e0f0305cd2c45d464
llj-notebook <1175637511@qq.com>
2022-03-24 23:25:11 +0800

    version one

--- app/src/main/java/com/android/main/MainActivity.kt
+++ app/src/main/java/com/android/main/MainActivity.kt
@@ -10,10 +10,10 @@ import com.huawei.hms.ml.scan.HmsScan
 import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
 import com.llj.baselib.IOTLib
 import com.llj.baselib.IOTViewModel
+import com.llj.baselib.save
 import com.llj.baselib.ui.IOTMainActivity
 import com.llj.baselib.utils.LogUtils
 import com.llj.baselib.utils.ToastUtils
-
 
 class MainActivity : IOTMainActivity<ActivityMainBinding>() {
 
@@ -24,13 +24,16 @@ class MainActivity : IOTMainActivity<ActivityMainBinding>() {
     override fun init() {
         super.init()
         initMainView()
-//        vm.connect(this, MainDataBean::class.java)
+        vm.connect(this, MainDataBean::class.java)
     }
 
     private fun initMainView() {
         initToolbar()
         mDataBinding.tvScanner.setOnClickListener {
             prepareScanner()
+        }
+        mDataBinding.tvCheck.setOnClickListener {
+            vm.turnOffLight()
         }
     }
 
@@ -67,7 +70,7 @@ class MainActivity : IOTMainActivity<ActivityMainBinding>() {
     }
 
     override fun webState(state: IOTViewModel.WebSocketType) {
-        mDataBinding.tvState.text = state.name
+        LogUtils.d(IOTLib.TAG, "state:${state.name}")
     }
 
     private fun prepareScanner() {
@@ -92,9 +95,18 @@ class MainActivity : IOTMainActivity<ActivityMainBinding>() {
             kotlin.runCatching {
                 val list = content.split(';')
                 val name = list[0]
-                val locationId = list[1].toInt()
+                val locationOrder = list[1]
                 val phoneNumber = list[2]
-                sendMessage(name,locationId,phoneNumber)
+                val ids = when (locationOrder) {
+                    "A" -> 1
+                    "C" -> 2
+                    "E" -> 3
+                    else -> -1
+                }
+                if (ids == -1) return
+                vm.sendOrder(locationOrder)
+                updateUI(name, ids, phoneNumber)
+                sendMessage(name, ids, phoneNumber)
                 LogUtils.d(IOTLib.TAG, "onActivityResult: 扫描结果为：$content")
             }.onFailure {
                 ToastUtils.toastShort("二维码信息有误")
@@ -103,20 +115,31 @@ class MainActivity : IOTMainActivity<ActivityMainBinding>() {
         }
     }
 
-    private fun sendMessage(name:String,id:Int,number:String){
-        val content = """智能快递分拣系统提醒您：${name}先生/女士，您的快递已放置到${id}号储物台，请及时领取"""
+    private fun updateUI(name: String, id: Int, number: String) {
+        val text = "${name}  ${id}号  ${number}\n"
+        mDataBinding.tvInfo.append(text)
+        val newNumber = mDataBinding.tvNumber.text.toString().toInt() + 1
+        mDataBinding.tvNumber.text = newNumber.toString()
+        IOTLib.getSP(RECORD_DATA).save {
+            putInt(RECORD_DATA_NUMBER, newNumber)
+            putString(RECORD_DATA_TV,mDataBinding.tvInfo.text.toString())
+        }
+    }
 
+    private fun sendMessage(name: String, id: Int, number: String) {
+        val content = """智能快递分拣系统提醒您：${name} 先生/女士，您的快递已放置到${id}号储物台，请及时领取"""
         val smstoUri: Uri = Uri.parse("smsto:")
         val intent = Intent(Intent.ACTION_VIEW, smstoUri)
         intent.putExtra("address", number) // 没有电话号码的话为默认的，即显示的时候是为空的
-
         intent.putExtra("sms_body", content) // 设置发送的内容
-
         intent.type = "vnd.android-dir/mms-sms"
         startActivity(intent)
     }
 
     companion object {
+        private const val RECORD_DATA = "RECORD_DATA"
+        private const val RECORD_DATA_TV = "RECORD_INFO_TV"
+        private const val RECORD_DATA_NUMBER = "RECORD_DATA_NUMBER"
         private const val SAO_CODE = 985
     }
 
--- app/src/main/java/com/android/main/MainVM.kt
+++ app/src/main/java/com/android/main/MainVM.kt
@@ -4,4 +4,22 @@ import com.llj.baselib.IOTViewModel
 
 class MainVM:IOTViewModel() {
 
+    private var order = ""
+
+    fun sendOrder(order:String){
+        this.order = order
+        sendOrderToDevice(order)
+    }
+
+    fun turnOffLight(){
+        val o = when(order){
+            "A"->"B"
+            "C" -> "D"
+            "E" -> "F"
+            else -> ""
+        }
+        if (o.isEmpty()) return
+        sendOrderToDevice(o)
+    }
+
 }
